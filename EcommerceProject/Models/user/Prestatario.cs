915c713da5b9e8b138548723af87da5eb07e1367
Nicolas Kornberg <nicokorn96@gmail.com>
2019-10-05 17:26:15 -0300

    cambio de carpetas

---
+++ EcommerceProject/Models/user/Prestatario.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EcommerceProject.Models
+{
+    public class Prestatario : User
+    {
+        
+    }
+}
