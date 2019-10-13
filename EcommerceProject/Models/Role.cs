94972ad0293c6cb106245865d56e2ad9805c274d
Nicolas <nicokorn96@gmail.com>
2019-10-13 20:28:30 -0300

    actualizado bootstrap,tabla de bd roles, creacion de abm de users
    actualizado bootstrap,tabla de bd roles, creacion de abm de users

---
+++ EcommerceProject/Models/Role.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EcommerceProject.Models
+{
+    public class Role
+    {
+        public int RoleId { get; set; }
+        public string Rolename { get; set; }
+    }
+}
