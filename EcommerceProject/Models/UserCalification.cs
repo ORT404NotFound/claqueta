d128caa411c4989a4ed0268b5b340da2e2029805
Nicolas <nicokorn96@gmail.com>
2019-10-13 17:36:05 -0300

    cambios en las calificaciones
    calificaciones division de 2 clases

---
+++ EcommerceProject/Models/UserCalification.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace EcommerceProject.Models
+{
+    public class UserCalification
+    {
+        [Key]
+        public int Id { get; set; }
+        public int Score { get; set; }
+        public String Comment { get; set; }
+        public String Replica { get; set; }
+        public virtual User User { get; set; }
+    }
+}
