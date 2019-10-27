271559fbb756eba3d5ecd303559a6c5295cf2d56
Nicolas Kornberg <nicokorn96@gmail.com>
2019-10-27 03:16:15 -0300

    cambios en la tabla intermedia

--- EcommerceProject/Models/UsuarioContratacion.cs
+++ EcommerceProject/Models/UsuarioContratacion.cs
@@ -9,12 +9,10 @@ namespace EcommerceProject.Models
 {
     public class UsuarioContratacion
     {
-        [Column(Order = 1), Key]
-        public int UsuarioId { get; set; }
-        [Column(Order = 2), Key]
-        public int ContratacionId { get; set; }
-        public virtual Usuario Usuario { get; set; }
+        [Key]
+        public int Id { get; set; }
         public virtual Contratacion Contratacion { get; set; }
+        public virtual Usuario Usuario { get; set; }
         public DateTime FechaDesde { get; set; }
         public DateTime FechaHasta { get; set; }
     }
