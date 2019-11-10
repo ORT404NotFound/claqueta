aa127080728a189ad91cc88058dec4c308eab2ae
Nicolas <nicokorn96@gmail.com>
2019-11-09 21:36:59 -0300

    admin migration

--- EcommerceProject/Migrations/MockData.cs
+++ EcommerceProject/Migrations/MockData.cs
@@ -26,22 +26,25 @@ namespace EcommerceProject.Migrations
                     }
                 }
                 db.SaveChanges();
+                Usuario us = db.Usuarios.Where(u => u.Email == "admin@claqueta.com.ar").FirstOrDefault();
+                if (us == null) {
+                    Usuario user = new Usuario();
+                    user.Activo = 1;
+                    user.Apellido = "Claqueta";
+                    user.Nombre = "Administrador";
+                    user.Password = "123123";
+                    user.ConfirmPassword = "123123";
+                    Rol rol = db.Roles.SingleOrDefault(r => r.Nombre == "ADMIN");
+                    user.Roles.Add(rol);
+                    user.Telefono = "48612121";
+                    user.TipoDocumento = "DNI";
+                    user.Documento = "111222333";
+                    user.Email = "admin@claqueta.com.ar";
+                    user.FechaDeNacimiento = Convert.ToDateTime(DateTime.Now);
+                    db.Usuarios.Add(user);
+                    db.SaveChanges();
 
-                Usuario user = new Usuario();
-                user.Activo = 1;
-                user.Apellido = "Claqueta";
-                user.Nombre = "Administrador";
-                user.Password = "123123";
-                user.ConfirmPassword = "123123";
-                Rol rol = db.Roles.SingleOrDefault(r => r.Nombre == "ADMIN");
-                user.Roles.Add(rol);
-                user.Telefono = "48612121";
-                user.TipoDocumento = "DNI";
-                user.Documento = "111222333";
-                user.Email = "admin@claqueta.com.ar";
-                user.FechaDeNacimiento = Convert.ToDateTime(DateTime.Now);
-                db.Usuarios.Add(user);
-                db.SaveChanges();
+                }
             }
 
          
